756b6d8fec4c481c0ffba773590e15ea905ac643
Kingslay <ybn24002@uconn.edu>
2025-11-18 19:17:54 -0500

    made code better

--- menu.py
+++ menu.py
@@ -1,120 +1,171 @@
 from tkinter import *
-import project10
 from PIL import Image, ImageTk
 
-#IMPORT PILLOW BEFORE RUNNING CUZ TKINTER IS ASS AND CANT READ SOME IMAGES
-#DO pip install Pillow OR NOTHING WILL WORK PROBABLY IDK
+class BudgetBuddyApp:
+    def __init__(self, menu):
+        self.menu = menu
+        self.menu.geometry("1024x768")
+        self.menu.title("Budget Buddy")
+
+        # temp images please ignore
+        self.temp_image1 = ImageTk.PhotoImage(Image.open("Images/jtempimage2.png").resize((200, 200)))
+        self.temp_image2 = ImageTk.PhotoImage(Image.open("Images/jtempimage1.png").resize((250, 250)))
+        self.title_photo = PhotoImage(file="Images/Budget_Icon.png")
+        self.menu.iconphoto(True, self.title_photo)
+
+        # Tracks currently displayed widgets
+        self.current_widgets = []
+
+        # Store user data
+        self.user_name = ""
+        self.new_category_name = ""
+
+        # Shows the screen where you input your name
+        self.show_name_input_screen()
+
+    # hides the current widgets on screen
+    def clear_screen(self):
+        for widget in self.current_widgets:
+            widget.pack_forget()
+        self.current_widgets = []
+
+    # Name input screen
+    def show_name_input_screen(self):
+        self.clear_screen()
+
+        self.title_label = Label(self.menu, 
+                                 text="Budget Buddy", 
+                                 font=("Arial",40,"bold"), 
+                                 fg="#ADD8E6",
+                                 relief=RAISED, 
+                                 bd=10, 
+                                 padx=20, 
+                                 pady=20)
+        self.title_label.pack()
+
+        self.name_label = Label(self.menu, 
+                                text="Enter your name.", 
+                                font=("Arial",20,"bold"))
+        self.name_label.pack()
+
+        self.name_box = Entry(self.menu)
+        self.name_box.pack()
+
+        self.submit_button = Button(self.menu, 
+                                    text="Submit", 
+                                    command=self.submit_name)
+        self.submit_button.pack()
+
+        self.current_widgets = [self.title_label, self.name_label, self.name_box, self.submit_button]
+
+    # Handles name submission
+    def submit_name(self):
+        self.user_name = self.name_box.get()
+        self.show_main_menu()
+
+    # Main menu screen
+    def show_main_menu(self):
+        self.clear_screen()
+
+        self.greeting = Label(self.menu, 
+                              text=f"Hello {self.user_name}, this is BudgetBuddy!", 
+                              font=("Arial",20,"bold"), 
+                              fg="#ADD8E6")
+        self.greeting.pack()
+
+        self.prompt = Label(self.menu, 
+                            text="What would you like to do?", 
+                            font=("Arial",20,"bold"))
+        self.prompt.pack()
+
+        self.btn_new_category = Button(self.menu, text="Create a new expense category", 
+                                       width=25, 
+                                       height=3,
+                                       command=self.show_new_category_screen)
+        
+        self.btn_add_expenses = Button(self.menu, 
+                                       text="Add expenses into a category", 
+                                       width=25, 
+                                       height=3)
+        
+        self.btn_display_expenses = Button(self.menu, 
+                                           text="Display expenses", 
+                                           width=25, 
+                                           height=3)
+        
+        self.btn_financial_status = Button(self.menu, 
+                                           text="View financial status", 
+                                           width=25, 
+                                           height=3)
+        
+        self.btn_save_expense = Button(self.menu, 
+                                       text="Save expenses", 
+                                       width=25, 
+                                       height=3)
+
+        for btn in [self.btn_new_category, self.btn_add_expenses, self.btn_display_expenses, 
+                    self.btn_financial_status, self.btn_save_expense]:
+            btn.pack(pady=5)
+
+        self.current_widgets = [self.greeting, self.prompt, self.btn_new_category, 
+                                self.btn_add_expenses, self.btn_display_expenses, 
+                                self.btn_financial_status, self.btn_save_expense]
+
+    # New category screen
+    def show_new_category_screen(self):
+        self.clear_screen()
+
+        self.label_new_category = Label(self.menu, 
+                                        text="Enter the name of the new category.", 
+                                        font=("Arial",20,"bold"))
+        self.label_new_category.pack()
+
+        self.entry_new_category = Entry(self.menu, 
+                                        width=25)
+        self.entry_new_category.pack()
+
+        self.confirm_btn = Button(self.menu, 
+                                  text="Confirm", 
+                                  width=20, 
+                                  height=3, 
+                                  command=self.confirm_new_category)
+        self.confirm_btn.pack()
+
+        self.current_widgets = [self.label_new_category, self.entry_new_category, self.confirm_btn]
+
+    # Confirm new category
+    def confirm_new_category(self):
+        self.new_category_name = self.entry_new_category.get()
+        self.show_category_confirmation()
+
+    # Confirmation screen
+    def show_category_confirmation(self):
+        self.clear_screen()
+
+        self.confirmation_label = Label(self.menu, 
+                                        text=f"{self.new_category_name} has been added as a category.", 
+                                        font=("Arial",20,"bold"))
+        self.confirmation_label.pack()
+
+        self.btn_add_another = Button(self.menu, 
+                                      text="Add another category?", 
+                                      width=25, 
+                                      height=3,
+                                      command=self.show_new_category_screen)
+
+        self.btn_return_menu = Button(self.menu, 
+                                      text="Return to Menu", 
+                                      width=25, 
+                                      height=3,
+                                      command=self.show_main_menu)
+
+        self.btn_add_another.pack(pady=5)
+        self.btn_return_menu.pack(pady=5)
+
+        self.current_widgets = [self.confirmation_label, self.btn_add_another, self.btn_return_menu]
 
 
-print(f"using this file{project10.__file__}")
-
-
-def submit_name():
-    #project10.main(name_box.get()) <--- Runs the main code from project10.py/commented out for testing speed reasons
-    adding_expenses = Label(menu,
-                            text=f"Hello {name_box.get()}, this is BudgetBuddy! Your personal Budgeting Assistant.",
-                            font=("Arial", 20, "bold"),
-                            fg="#ADD8E6",
-                            relief=RAISED,
-                            bd=10,
-                            padx=20,
-                            pady=20, 
-                            compound="top")
-    adding_expenses.pack()
-    
-    text_label1 = Label(menu,
-                        text="What would you like to do?",
-                        font=("Arial", 20, "bold"),
-                        )
-    text_label1.pack()
-
-    new_expense_category = Button(menu,
-                         text="Create a new expense category",
-                         width=25,
-                         height=5)
-    new_expense_category.pack()
-
-    add_expenses = Button(menu,
-                         text="Add expenses into a category",
-                         width=25,
-                         height=5)
-    add_expenses.pack()
-
-    display_expenses = Button(menu,
-                              text="Display expenses",
-                              width=25,
-                              height=5)
-    display_expenses.pack()
-
-    financial_status = Button(menu,
-                              text="View financial status",
-                              width=25,
-                              height=5)
-    financial_status.pack()
-
-    save_expense = Button(menu,
-                              text="View financial status",
-                              width=25,
-                              height=5)
-    save_expense.pack()
-
-
-    #hides all the labels that were orginally present so we don't need to make new windows repeatdly
-    title.pack_forget()
-    name_label.pack_forget()
-    name_box.pack_forget()
-    submit_button.pack_forget()
-
-
-
-menu = Tk()
-
-
-menu.geometry("1024x768")
-menu.title("Budget Buddy")
-
-img1 = Image.open("Images/jtempimage2.png").resize((200, 200))
-img2 = Image.open("Images/jtempimage1.png").resize((250, 250))
-menu.temp_image1 = ImageTk.PhotoImage(img1)
-menu.temp_image2 = ImageTk.PhotoImage(img2)
-
-
-
-title_photo = PhotoImage(file="Images\Budget_Icon.png")
-smaller_title_photo = title_photo.subsample(2, 2) #reduces size of the image
-menu.iconphoto(True, title_photo)
-
-title = Label(menu, 
-              text="Budget Buddy", 
-              font=("Arial",40,"bold"), 
-              fg="#ADD8E6",
-              relief=RAISED,
-              bd=10,
-              padx=20,
-              pady=20, 
-              compound="top")
-title.pack()
-
-name_label = Label(menu,
-                   text="Enter your name.",
-                   font=("Arial",20,"bold"))
-name_label.pack()
-
-
-name_box = Entry(menu)
-name_box.pack()
-
-submit_button = Button(menu,
-                       text="Submit",
-                       command=submit_name)
-submit_button.pack()
-
-#Gui Elements after the name input screen
-
-
-
-
-
-
-
-menu.mainloop()
+if __name__ == "__main__":
+    menu = Tk()
+    app = BudgetBuddyApp(menu)
+    menu.mainloop()
--- project10.py
+++ project10.py
@@ -3,26 +3,16 @@ from tkinter import *
 from library import functions
 from library.classes_10 import Budget
 
-def main(name):
+def main(budget_name):
 
-    print(f"Hey {name}, this is BudgetBuddy! Your personal Budgeting Assistant.")
+    budget_category = {}
 
-    instances = {}
-
-    grocery = Budget("Grocery")
-    Toyota_Camry = Budget("Car")
-
-    grocery.add_expenses()
-    Toyota_Camry.add_expenses()
-
-    grocery.get_expense_details()
-    Toyota_Camry.get_expense_details()
-
-    Budget.calc_total_balance(grocery, Toyota_Camry)
-
+    
+    budget_category[budget_name] = Budget(budget_name)
+  
 
 if __name__ == "__main__":
-    main("Kingslay")
+    main()
 
 
 
